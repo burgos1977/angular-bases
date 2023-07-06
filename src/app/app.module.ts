3ff9a5a443be75179947a3d5a34ccd6958c911c4
Mauricio Burgos <mauricio.burgos@pfcti.com>
2023-07-06 11:17:48 -0600

    Fin de sección 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
+// import { CounterComponent } from './counter/components/counter/counter.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
+
 import { AppComponent } from './app.component';
+import { HeroComponent } from './heroes/components/hero/hero.component';
+// import { HeroComponent } from './heroes/hero/hero.component';
+// import { ListComponent } from './heroes/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // CounterComponent,
+    // HeroComponent,
+    // ListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CounterModule,
+    HeroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
